cf012f1ba568a751f156065b842914313538a2fe
putrisalwael@gmail.com <you@example.com>
2022-04-17 23:16:12 +0700

    first commit

---
+++ app/Http/Controllers/CobaController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Friends;
+
+class CobaController extends Controller
+{
+    public function index()
+    {
+        $friends = Friends::paginate(3);
+
+        return view('index', compact('friends'));
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        // Validate the request...
+        
+        $friends=new Friends;
+
+        $friends->nama = $request->nama;
+        $friends->no_hp = $request->no_hp;
+        $friends->alamat = $request->alamat;
+
+        $friends->save();
+
+    }
+}
---
+++ resources/views/coba.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Cobaaaaaa')
+
+@section('content')
+    Urutan ke - {{$ke}}
+@endsection
