74b7805b814c8317dcf626fb23d8ddef92611369
putrisalwael@gmail.com <you@example.com>
2022-05-16 15:11:15 +0700

    commit

---
+++ resources/views/Groups/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Cobaaaaaa')
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+        <h3>Nama Group : {{ $groups['name'] }}</h3>
+        <h3>Description : {{ $groups['description'] }}</h3>
+        <h3>Jumlah anggota : 3</h3>
+        <h3>Jumlah anggota yang pernah masuk : 3</h3>
+        <h3>Jumlah anggota yang keluar: </h3>
+        </div>
+    </div>
+@endsection
